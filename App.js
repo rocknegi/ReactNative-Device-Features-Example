07457e9fe5c6e89edbef948258b3886f96439e12
rocknegi <rocknegi53@gmail.com>
2019-09-30 13:01:30 +0530

    set up react-navigation

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator } from 'react-navigation-stack'
 
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
+import Home from './components/Home'
+
+const AppNavigator = createStackNavigator ({
+  Home:{
+    screen:Home
   }
-}
+},{
+  initialRouteName: 'Home',
+  headerMode: 'none',
+})
+export default createAppContainer(AppNavigator)
