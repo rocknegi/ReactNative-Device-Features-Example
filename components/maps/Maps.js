a93983b0f9e963d4b55450dc6fbf88b92056de3c
rocknegi <rocknegi53@gmail.com>
2019-10-04 22:25:52 +0530

    add marker and live location in maps

--- components/maps/Maps.js
+++ components/maps/Maps.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Dimensions, StyleSheet } from 'react-native'
-import { Container, Content } from 'native-base'
+import { Container, Content, Icon, Button } from 'native-base'
 import MapView from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
@@ -8,8 +8,8 @@ import RetroMapStyles from './RetroMapStyles.json';
 
 const LATITUDE = 0;
 const LONGITUDE = 0;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = 0.0421;
+const LATITUDE_DELTA = 0.0122;
+const LONGITUDE_DELTA = Dimensions.get('window').width / Dimensions.get('window').height * 0.0122;
 
 export default class Maps extends Component {
     state = {
@@ -18,25 +18,53 @@ export default class Maps extends Component {
             longitude: LONGITUDE,
             latitudeDelta: LATITUDE_DELTA,
             longitudeDelta: LONGITUDE_DELTA,
-        }
+        },
+        locationChosen: false
     }
     componentDidMount() {
+        this.getCurrentPosition()
+    }
+    getCurrentPosition = () =>
         Geolocation.getCurrentPosition(
             position => {
-                this.setState({
-                    region: {
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                        latitudeDelta: LATITUDE_DELTA,
-                        longitudeDelta: LONGITUDE_DELTA,
+                const coordsEvent = {
+                    nativeEvent: {
+                        coordinate: {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude
+                        }
                     }
-                });
+                }
+                this.pickedLocationHandler(coordsEvent)
             },
             (error) => console.log(error.message),
             { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
         );
+
+    pickedLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.map.animateToRegion({
+            ...this.state.region,
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        })
+        this.setState(prevState => {
+            return {
+                region: {
+                    ...prevState.region,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                },
+                locationChosen: true
+            }
+        })
+
     }
     render() {
+        let marker = null;
+        if (this.state.locationChosen) {
+            marker = <MapView.Marker coordinate={this.state.region} />
+        }
         return (
             <Container style={{ height: Dimensions.get('window').height / 1.3, }}>
                 <Content>
@@ -45,11 +73,20 @@ export default class Maps extends Component {
                             width: '100%',
                             height: Dimensions.get('screen').height
                         }}
+                        initialRegion={this.state.region}
+                        onPress={this.pickedLocationHandler}
                         customMapStyle={RetroMapStyles}
                         showsUserLocation={true}
-                        region={this.state.region}
-                    />
+                        ref={ref => this.map = ref}
+                    >
+                        {marker}
+                    </MapView>
                 </Content>
+                <Icon
+                style={{fontSize:40,left:'85%'}}
+                    onPress={this.getCurrentPosition}
+                    type="MaterialIcons"
+                    name="my-location" />
             </Container>
         )
     }
